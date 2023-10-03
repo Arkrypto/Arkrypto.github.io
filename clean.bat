fa46297c8331f34a9fe304c0dd9e235808fbfd19
northboat <northboat@163.com>
2023-10-03 15:00:23 +0800

    update

--- clean.bat
+++ clean.bat
@@ -1,11 +1,11 @@
-rd /s /q 408
-rd /s /q lib
 rd /s /q assets
-rd /s /q cs
-rd /s /q dev
-rd /s /q img
-rd /s /q song
-rd /s /q js
+rd /s /q categories
 rd /s /q css
+rd /s /q docs
+rd /s /q img
+rd /s /q js
+rd /s /q song
+rd /s /q tag
+rd /s /q timeline
 del -q 404.html
 del -q index.html
