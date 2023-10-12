7179d5443a96098db9c658bbec438b32e9fec125
northboat <northboat@163.com>
2023-10-12 15:36:06 +0800

    change img

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,9 @@
-xcopy /s /e /i /y VuePress-Reco/public northboat.github.io
-rd /s /q VuePress-Reco/public
+%放在上一级目录下执行，不得不吐槽一句这个反人类的反斜%
+xcopy /s /e /i /y VuePress-Reco\public canoe95.github.io
+rd /s /q VuePress-Reco\public
 
 
-cd northboat.github.io
+cd canoe95.github.io
 call push
 
 pause
