81526f126de06f2d2a45f6b47331de7acd55e546
northboat <northboat@163.com>
2023-11-22 23:17:11 +0800

    update title

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,8 @@
 %放在上一级目录下执行，不得不吐槽一句这个反人类的反斜%
-xcopy /s /e /i /y VuePress-Reco\public canoe95.github.io
-rd /s /q VuePress-Reco\public
+
+
+xcopy /s /e /i /y VuePress-Reco-1.x\public canoe95.github.io
+rd /s /q VuePress-Reco-1.x\public
 
 
 cd canoe95.github.io
