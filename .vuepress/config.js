485ea68d01e7e84bb0b86fd79a49951b935314ab
canoe <northboat@163.com>
2024-02-05 17:32:47 +0800

    update discrete

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -144,7 +144,7 @@ module.exports = {
 		],*/
 		[ '@vuepress-reco/vuepress-plugin-bulletin-popover', {
 			width: '240px', // 默认 260px
-			title: '真是美美又丽丽哪',
+			title: '真是美美又丽丽呐',
 			body: [{
 				type: 'image',
 				src: '/img/hu318.png'
