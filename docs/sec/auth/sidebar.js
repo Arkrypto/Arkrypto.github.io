b7b6dd468587577c9f8426df06dcd164a06a7d1c
canoe <northboat@163.com>
2024-07-04 01:43:04 +0800

    update totp-auth

--- docs/sec/auth/sidebar.js
+++ docs/sec/auth/sidebar.js
@@ -3,7 +3,8 @@ module.exports = [
 		title: '双向认证',
 		collapsable: true,
 		children: [
-			'./p2p/ukey-sign-server-verify',
+			'./p2p/2fa-bank-ukey-auth',
+			'./p2p/2fa-github-totp-auth',
 		]
 	},
 ]
