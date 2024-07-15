d8fb6f71809504d6f6997fb672779269c6fea5c1
canoe <northboat@163.com>
2024-07-16 04:12:37 +0800

    update

--- public/js/clock.js
+++ public/js/clock.js
@@ -34,7 +34,7 @@ function countdown(date){
 	//alert(loc)
 	//console.log(loc)
 	
-	if(loc == "/docs/xdu/math/"){
+	if(loc == "/docs/sec/lib/"){
 		let diff = getDiff(d)
 		document.getElementById("countdown").innerHTML = diff;	
 	}
