c7853e4c49807a41d32a75efe8883c92d3f9cb47
canoe <northboat@163.com>
2024-07-29 01:36:14 +0800

    update

--- docs/sec/auth/sidebar.js
+++ docs/sec/auth/sidebar.js
@@ -9,22 +9,15 @@ module.exports = [
 	},
 	
 	{
-		title: '可搜索加密',
+		title: '端到端加密',
 		collapsable: true,
 		children: [
-			'./se/se-emulation',
+			'./p2p/p2p-searchable-encryption',
+			//'./p2p/https',
+			//'./p2p/websocket',
 		]
 	},
-	
-	/*{
-		title: '端到端认证',
-		collapsable: true,
-		children: [
-			'./p2p/https',
-			'./p2p/websocket',
-		]
-	},
-	
+	/*
 	{
 		title: 'RFID 认证',
 		collapsable: true,
