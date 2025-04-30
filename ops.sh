a7ab6e0cdb47f78c865caaf84e82ed55aa713649
canoe <northboat@163.com>
2025-04-30 22:25:43 +0800

    update: add ipfe tree[D[D[D[-[C[C[C[C and sort directory

--- ops.sh
+++ ops.sh
@@ -11,51 +11,46 @@ function print_menu() {
 	# clear  # 清屏使界面更清爽（可选）
     echo -e "\033[34m===================================\033[0m"
 	echo -e "\033[32m[The Operations of This Repository]:\033[0m"
-    echo "1.dev"
-    echo "2.build"
-    echo "3.push"
-	echo "4.push again"
-    echo "5.pull"
+    echo "1.npm run dev"
+    echo "2.npm run build"
+    echo "3.git add && commit && push"
+	echo "4.git push"
+    echo "5.git pull"
     echo -e "\033[34m===================================\033[0m"
+}
+
+print_cat(){
+	clear
+    echo ""
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+    echo ""
+	echo "		            ／\\___________／\\"
+	echo "		           /      o   o      \\"
+	echo "		         (     =    ᴥ    =     )"
+	echo "		        (                       )"
+	echo "		       (    (   (       )   )    )"
+	echo "		      (____(__)_____(___(__)______)"
+	
 }
 
 # 函数：显示退出动画
 exit_animation() {
     clear  # 清屏
 
-    # 大肥猫的图案
-    cat_art=(
-        "      /\\_____/\\  "
-        "     /  o   o  \\ "
-        "    ( ==  ^  == )"
-        "     )         ( "
-        "    (           )"
-        "   ( (  )   (  ) )"
-        "  (__(__)___(__)__)"
-    )
-
     # 显示大肥猫和消息
-    for i in {1..2}; do
-        clear
-        echo ""
-        for line in "${cat_art[@]}"; do
-            echo "$line"
-        done
-        echo ""
-        echo "   miao~miao~miao~"
-        sleep 2 # 停留 2 秒
+    print_cat	
+    echo ""
+    echo "		            miao wu~ miao wu~"
+    sleep 2 # 停留 2 秒
 
-        clear
-        echo ""
-        for line in "${cat_art[@]}"; do
-            echo "$line"
-        done
-        echo ""
-        echo "  see you next day~"
-        sleep 2  # 停留 2 秒
-    done
-
-    sleep 1  # 最后停留1秒
+	print_cat
+    echo ""
+    echo "		            see you next day~"
+    sleep 3  # 停留 3 秒
 }
 
 
@@ -63,7 +58,7 @@ while true; do
     
 	print_menu
     
-    read -p "enter the operation (1-4, or 0 to exit): " n
+    read -p "enter the operation (1-5, or 0 to exit): " n
 
     case $n in
         1) 
@@ -83,8 +78,9 @@ while true; do
 			[[ $confirm == [yY] ]] && git pull || echo "已取消"
 			;;
 		0)
-			exit_animation 
-			read -p "是否需要关机呢？(y/n) " confirm
+			exit_animation
+			echo ""
+			read -p "		        需要大肥猫帮你关机吗(y/n) " confirm
 			[[ $confirm == [yY] ]] && shutdown -s -t 10 && echo "将在十秒后关机" || exit 0  # 退出脚本
 			;;
         *)
